6213c2a3df4a115d1faef0c7d680e9014a3c0f95
sunnyok03 <sunnyok03@gmail.com>
2023-12-03 21:02:52 +0530

    Refactor added constructor for MoodAnalyzer class

--- src/com/bridgelabz/Main.java
+++ src/com/bridgelabz/Main.java
@@ -2,7 +2,7 @@ package com.bridgelabz;
 
 public class Main {
     public static void main(String[] args) {
-        MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
-        System.out.println(moodAnalyzer.analyzeMood("I am happy."));
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer("I am happy.");
+        System.out.println(moodAnalyzer.analyzeMood());
     }
 }
--- src/com/bridgelabz/MoodAnalyzer.java
+++ src/com/bridgelabz/MoodAnalyzer.java
@@ -1,8 +1,28 @@
 package com.bridgelabz;
-
+/*
+Analyzes mood whether a message is happy or sad
+ */
 public class MoodAnalyzer {
+    private String message;
 
-    public String analyzeMood(String message){
+    /*
+    default constructor
+     */
+    public MoodAnalyzer(){}
+
+    /*
+    parameterised constructor
+     */
+    public MoodAnalyzer(String message){
+        this.message = message;
+    }
+
+    /*
+    @desc: analyzeMood with no parameter, uses message field
+    @params:
+    @return: String indicating "SAD" or "HAPPY" message
+     */
+    public String analyzeMood(){
         message = message.toLowerCase();
         if(message.contains("sad")){
             return "SAD";
